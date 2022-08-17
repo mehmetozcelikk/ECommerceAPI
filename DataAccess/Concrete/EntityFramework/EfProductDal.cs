53c4e7bcfeb1b8b0d9a22c264b0420f4bf547f19
Mehmet Özçelik <mehmett.ozcelik@yahoo.com>
2022-08-17 14:10:17 +0300

    update

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -2,6 +2,7 @@
 using Entities.Concrete;
 using Entities.ECommerceDTO;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace DataAccess.Concrete.EntityFramework
 {
@@ -9,15 +10,46 @@ namespace DataAccess.Concrete.EntityFramework
     {
         public List<Product> GetProduct(string Name, string CategoryName, string ProductAttributes, string PriceRange)
         {
-            {
-                using (var context = new DataContext())
-                {
-                    var productvalue = context.Products.Include(i => i.ProductCategory).Where(x => x.Name == Name || x.ProductCategory.Name == CategoryName ||
-                                                    x.ProductCategory.Color == ProductAttributes || x.ProductCategory.Size == ProductAttributes).ToList();
+            //if (null != ProductAttributes)
+            //{
+            //    using (var context = new DataContext())
+            //    {
+            //        var productvalue = context.Products.Include(i => i.ProductCategory).Where(x => x.Name == Name || x.ProductCategory.Name == CategoryName ||
+            //                                        x.ProductCategory.Color == ProductAttributes || x.ProductCategory.Size == ProductAttributes
+            //                                        || x.Price <= Convert.ToUInt64(PriceRange)).ToList();
 
-                    return productvalue;
-                }
+            //        return productvalue;
+            //    }
+            //}
+
+            //using (var context = new DataContext())
+            //{
+            //    var productvalue = context.Products.Include(i => i.ProductCategory).Where(x => x.Name == Name || x.ProductCategory.Name == CategoryName                                                 
+            //                                    || x.Price <= Convert.ToUInt64(PriceRange)).ToList();
+
+            //    return productvalue;
+            //}
+
+            var context2 = new DataContext();
+            IQueryable<Product> query = context2.Products;
+            query = query.Include(x => x.ProductCategory);
+
+            if (null != Name) 
+              query = query.Where(x => x.Name == Name);
+            
+            if (null != CategoryName)
+                query = query.Where(x => x.ProductCategory.Name == CategoryName);
+
+            if (null != ProductAttributes)
+            {
+                query = query.Where(x => x.ProductCategory.Size == ProductAttributes || x.ProductCategory.Color == ProductAttributes);
             }
+
+            if (null != PriceRange)
+                query = query.Where(x => x.Price <= Convert.ToInt64(PriceRange));
+
+            return query.ToList();
+
         }
     }
 }
